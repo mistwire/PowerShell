ad177fc34d679514222c73fa08aa1795bbaae689
Chris Williams <chrisfwilliams@gmail.com>
2017-10-22 09:36:51 -0400

    asdf

--- BCG/BCGchangeIP.ps1
+++ BCG/BCGchangeIP.ps1
@@ -51,4 +51,30 @@ Invoke-VMScript -VM $VMname -GuestUser $VMUSER -GuestPassword $VMPASS -ScriptTyp
 #Invoke-VMScript -VM $VMname -GuestUser $VMUSER -GuestPassword $VMPASS -ScriptType bat -ScriptText $netsh4
 #Invoke-VMScript -VM $VMname -GuestUser $VMUSER -GuestPassword $VMPASS -ScriptType bat -ScriptText $netsh5
 Write-Host "Setting IP address completed." -ForegroundColor Green
+
+Write-Host "Modifying DNS host file for $VMname..." -ForegroundColor Yellow
+(Get-Content "$($env:windir)\system32\Drivers\etc\hosts")  -replace ('^\s*164.22.247.10\s+binbap1bka',' ') |
+Out-File "$($env:windir)\system32\Drivers\etc\hosts" -Force
+(Get-Content "$($env:windir)\system32\Drivers\etc\hosts")  -replace ('^\s*164.22.247.11\s+bim1s010bka',' ') |
+Out-File "$($env:windir)\system32\Drivers\etc\hosts" -Force
+(Get-Content "$($env:windir)\system32\Drivers\etc\hosts")  -replace ('^\s*164.22.247.12\s+bim2s010bka',' ') |
+Out-File "$($env:windir)\system32\Drivers\etc\hosts" -Force
+(Get-Content "$($env:windir)\system32\Drivers\etc\hosts")  -replace ('^\s*164.22.247.13\s+bim1s020bka',' ') |
+Out-File "$($env:windir)\system32\Drivers\etc\hosts" -Force
+(Get-Content "$($env:windir)\system32\Drivers\etc\hosts")  -replace ('^\s*164.22.247.14\s+bim1s030bka',' ') |
+Out-File "$($env:windir)\system32\Drivers\etc\hosts" -Force
+(Get-Content "$($env:windir)\system32\Drivers\etc\hosts")  -replace ('^\s*164.22.247.15\s+bim2s020bka',' ') |
+Out-File "$($env:windir)\system32\Drivers\etc\hosts" -Force
+(Get-Content "$($env:windir)\system32\Drivers\etc\hosts")  -replace ('^\s*164.22.247.16\s+bim2s030bka',' ') |
+Out-File "$($env:windir)\system32\Drivers\etc\hosts" -Force
+(Get-Content "$($env:windir)\system32\Drivers\etc\hosts")  -replace ('^\s*160.118.160.24\s+tsadm311',' ') |
+Out-File "$($env:windir)\system32\Drivers\etc\hosts" -Force
+(Get-Content "$($env:windir)\system32\Drivers\etc\hosts")  -replace ('^\s*160.118.160.21\s+tsadm121',' ') |
+Out-File "$($env:windir)\system32\Drivers\etc\hosts" -Force
+(Get-Content "$($env:windir)\system32\Drivers\etc\hosts")  -replace ('^\s*160.118.160.23\s+tsadm221',' ') |
+Out-File "$($env:windir)\system32\Drivers\etc\hosts" -Force
+
+Write-Host "Uninstalling Nagios client for $VMname..." -ForegroundColor Yellow
+$nagiosuninst = 
+Invoke-VMScript -VM $VMname -GuestUser $VMUSER -GuestPassword $VMPASS -ScriptType bat -ScriptText $nagiosuninst
 }
